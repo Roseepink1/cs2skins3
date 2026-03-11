f94f5a48198bd2131a8d2ff82cdf2c1927d283e4
Roseepink1 <95916578+Roseepink1@users.noreply.github.com>
2026-03-11 11:33:43 +0800

    upload website files

---
+++ class/config.php
@@ -0,0 +1,16 @@
+<?php
+define('SKIN_LANGUAGE', 'skins_en');
+
+define('DB_HOST', 'hopper.proxy.rlwy.net');
+define('DB_PORT', '59939');
+define('DB_NAME', 'railway');
+define('DB_USER', 'root');
+define('DB_PASS', 'ssywjzAhnrTiTlfzppHMaSinvuOeQaoj');
+
+define('WEB_STYLE_DARK', true);
+
+define('STEAM_API_KEY', '166B19289797C07CCFCFC711FCCCA642');
+define('STEAM_DOMAIN_NAME', '');
+define('STEAM_LOGOUT_PAGE', '');
+define('STEAM_LOGIN_PAGE', '');
+
