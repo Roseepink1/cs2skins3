ee8be99135b1b416c980baeec6dceb14d8cadc94
Roseepink1 <95916578+Roseepink1@users.noreply.github.com>
2026-03-14 00:15:54 +0800

    Update index.php

--- index.php
+++ index.php
@@ -123,7 +123,7 @@ if (isset($_SESSION['steamid'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>CS2 Simple Weapon Paints</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css?v=<?php echo time(); ?>">
 </head>
 
 <body>
